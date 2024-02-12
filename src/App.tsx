b06ecc3321a9db5b70c643da3003e4d1dcc2eb17
Nicole Fazal <108958417+NJFazal@users.noreply.github.com>
2024-02-12 10:54:30 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { Questions } from './types';
 import { useState } from 'react';
 import StatBar from './components/StatBar';
 import QuestionComp from './components/Question';
-import Reset from './components/Reset'
+import Reset from './components/Reset';
+import Classnames from 'classnames';
 
 function App() {
     const allQuestions = questions as Questions;
@@ -13,38 +14,43 @@ function App() {
     const [correctAnswers, setCorrectAnswers] = useState(0);
     const [incorrectAnswers, setIncorrectAnswers] = useState(0);
 
-    const [waitingToAdvance, setWaitingToAdvance] = useState(false)
+    const [waitingToAdvance, setWaitingToAdvance] = useState(false);
     const onSubmit = (correct: boolean) => {
-        if (correct) setCorrectAnswers(correctAnswers + 1)
-        else setIncorrectAnswers(incorrectAnswers + 1)
-    setWaitingToAdvance(true)
-    }
+        if (correct) setCorrectAnswers(correctAnswers + 1);
+        else setIncorrectAnswers(incorrectAnswers + 1);
+        setWaitingToAdvance(true);
+    };
     const advance = () => {
-        setWaitingToAdvance(false)
-        setCurrentQuestionIdx(currentQuestionIdx + 1)
-    }
+        setWaitingToAdvance(false);
+        setCurrentQuestionIdx(currentQuestionIdx + 1);
+    };
     const reset = () => {
-        setCurrentQuestionIdx(0)
-        setCorrectAnswers(0)
-        setIncorrectAnswers(0)
-        setWaitingToAdvance(false)
-    }
-    if (currentQuestionIdx >= allQuestions.questions.length) return <Reset totalQuestions={allQuestions.questions.length} 
-           correctQuestions={correctAnswers}
-           onPress={reset}
-    />
+        setCurrentQuestionIdx(0);
+        setCorrectAnswers(0);
+        setIncorrectAnswers(0);
+        setWaitingToAdvance(false);
+    };
+    if (currentQuestionIdx >= allQuestions.questions.length)
+        return (
+            <Reset
+                totalQuestions={allQuestions.questions.length}
+                correctQuestions={correctAnswers}
+                onPress={reset}
+            />
+        );
     return (
-        <div className={styles['stat-container']}>
+        <div className={Classnames(styles['stat-container'], styles.frame)}>
             <StatBar
                 currentQuestion={currentQuestionIdx + 1}
                 totalQuestions={allQuestions.questions.length}
                 correct={correctAnswers}
                 incorrect={incorrectAnswers}
             />
-            <QuestionComp 
-            question={ allQuestions.questions[currentQuestionIdx]} onSubmit={onSubmit}
+            <QuestionComp
+                question={allQuestions.questions[currentQuestionIdx]}
+                onSubmit={onSubmit}
             />
-            { waitingToAdvance && <button onClick={advance}>Next Question...</button>}
+            {waitingToAdvance && <button onClick={advance}>Next Question...</button>}
         </div>
     );
 }
--- src/components/Answer.tsx
+++ src/components/Answer.tsx
@@ -1,16 +1,23 @@
+import Answer_module from './Answer.module.scss';
+import Answers_module from './Answers.module.scss';
+import Classnames from 'classnames';
 type Props = {
-    onPress: () => void
+    onPress: () => void;
     text: string;
     color?: string;
     disabled?: boolean;
-}
+};
 function Answer(props: Props) {
-    const style = props.color?{ color: props.color } : {};
+    const style = props.color ? { color: props.color } : {};
     return (
-    <button onClick={props.onPress} disabled={ props.disabled}>
-        <span style={style}>{props.text}</span>
+        <button
+            onClick={props.onPress}
+            disabled={props.disabled}
+            className={Classnames(Answer_module.button, Answers_module.choices)}
+        >
+            <span style={style}>{props.text}</span>
         </button>
-)
+    );
 }
 
-export default Answer
+export default Answer;
